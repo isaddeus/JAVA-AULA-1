2bb4d949ef0d3028522017df32027092dea31950
Laboratório FIAP <isadedeus0417@gmail.com>
2025-04-10 22:35:27 -0300

    AULA 2 JAVA

---
+++ app.js
@@ -0,0 +1,39 @@
+
+//let nome = prompt ("isabela")
+// const apiKey = "lalala"
+
+//if (nome ==="Isabela" || nome === null || nome === "") {
+//    console.log("É você!")
+//}
+
+//else if (nome === "Fulano") {
+//    console.log("Outro mano.")
+//}
+
+//else {
+//    console.log("Não é voce")
+//}
+
+// crase + texto fixo + ${} que é o texto variavel
+
+//let nome = prompt("Insira seu usuário: ")
+//let senha = prompt("Insira a sua senha: ")
+
+//if (nome === '1234' || senha ==='1234') {
+//    alert("Login  efetuado com sucesso.")
+//}
+//else {
+//    alert("Usuário ou senha incorretos.")
+//}
+"bela"||"gabi"||"arthur morgan"
+"1234"||"4321"||"1989"
+
+let user = prompt("Insira seu usuário: ")
+let senha = prompt("Insira sua senha: ")
+
+if (
+    ( user === "bela" && senha == "1234")||
+    ( user === "gabi" && senha == "4321")||
+    ( user === "arthur morgan" && senha == "1989")|| {
+    ) 
+} 
