8711df167f0901691cd84c5122eff29023559e8a
isabela <isadedeus0417@gmail.com>
2025-04-24 22:41:21 -0300

    alterações sala CHECAR SLIDES

--- app.js
+++ app.js
@@ -14,7 +14,7 @@
 //    console.log("Não é voce")
 //}
 
-// crase + texto fixo + ${} que é o texto variavel
+// ------------- crase + texto fixo + ${} que é o texto variavel ---------------- 
 
 //let nome = prompt("Insira seu usuário: ")
 //let senha = prompt("Insira a sua senha: ")
@@ -40,13 +40,58 @@
 //    ) 
 //} 
 
-switch(nome){
-    case "isabela":
-        console.log("é voce")
-        break
-    case "fulano":
-        console.log("Você é fulano")
-        break
-    default:
-        console.log("Não te conheço")
+//switch(nome){
+//    case "isabela":
+//        console.log("é voce")
+//        break
+//    case "fulano":
+//        console.log("Você é fulano")
+//        break
+//   default:
+//        console.log("Não te conheço")
+//}
+
+// AULA 2
+
+//function pegaNome() {
+//    let nome = "isabela"
+//    let sobrenome = ""
+//    return nome
+
+//}
+
+//function exibirNome(nomeUsuario) {
+//    nomeUsuario
+//    let resultado = pegaNome()
+//    let nome = ""
+//    console.log(resultado)
+//}
+
+//exibirNome("isabela", "deDeus")
+
+
+// crie uma função:
+// recebe nome do usuario|outra recebe idade do usuario|outra exibe uma mensagem com os dois|outra pra iniciar o programa e chamar as outras funções
+
+//function pegaNome() {
+//    let nome = prompt("Informe seu nome: ")
+//    return nome
+//}
+
+//function idade() {
+//   let idade = prompt("Informe a sua idade: ")
+//   return idade
+//}
+
+//pegaNome()
+//idade()
+
+// ------------ PASSO A PASSO -----------------
+// 1. define a função + nome
+function nomeRandom() {
+    // 2. informe o que a função deve fazer
+    let nome2 = prompt("Informe o seu nome: ")
+    let idade2 = prompt("Agora, informe a sua idade: ") // console log mostra o que foi pedido no 'prompt'
 }
+// 3. Rode a função (nome da função + ())
+alert(nomeRandom())
